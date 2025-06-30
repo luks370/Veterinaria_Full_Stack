8a72199534a03218c9f56851169370c1dacd89a9
Lucas Eduardo Ovejero <cursos849@gmail.com>
2025-06-29 23:10:41 -0300

    controller: actualizarPaciente eliminarPaciente

--- Back-End/Controllers/pacientes.js
+++ Back-End/Controllers/pacientes.js
@@ -41,23 +41,96 @@ const obtenerPaciente = async (req, res) => {
     }
 }
 
-const agregar = async (req, res) => {
-    const {propietario, nombre, email, telefono, fecha, sintomas} = req.body;
+const agregarPaciente = async (req, res) => {
+  const { propietario, nombre, email, telefono, fecha, sintomas } = req.body;
 
-    try {
-        const pacienteNuevo = await pacientesTabla.create({propietario, nombre, email, telefono, fecha, sintomas, veterinarioId: req.veterinario.id})
+  try {
+    const pacienteNuevo = await pacientesTabla.create({
+      propietario,
+      nombre,
+      email,
+      telefono,
+      fecha,
+      sintomas,
+      veterinarioId: req.veterinario.id,
+    });
 
-        res.status(200).json(pacienteNuevo)
+    res.status(200).json(pacienteNuevo);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ msj: "error al agregar" });
+  }
+};
 
-    } catch (error) {
-        console.log(error)
-        res.status(404).json({msj: "error al agregar"})
+const actualizarPaciente = async (req, res) => {
+  try {
+    const {
+      propietario,
+      nombre,
+      email,
+      telefono,
+      fecha,
+      sintomas,
+      veterinarioId,
+    } = req.body;
+
+    const { id } = req.params;
+
+    const paciente = await pacientesTabla.findByPk(id);
+
+    if (!paciente) {
+      return res.status(404).json({ msj: "No existe paciente" });
     }
-}
 
+    if (paciente.veterinarioId != req.veterinario.id) {
+      return res
+        .status(404)
+        .json({ msj: "No puedes modificar un paciente que no es tuyo" });
+    }
 
- 
+    paciente.propietario = propietario || paciente.propietario;
+    paciente.nombre = nombre || paciente.nombre;
+    paciente.email = email || paciente.email;
+    paciente.telefono = telefono || paciente.telefono;
+    paciente.fecha = fecha || paciente.fecha;
+    paciente.sintomas = sintomas || paciente.sintomas;
 
-export {obtenerPacientes, obtenerPaciente,
-    agregar
-}
+    await paciente.save();
+
+    return res.status(200).json(paciente);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const eliminarPaciente = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const paciente = await pacientesTabla.findByPk(id);
+
+    if (!paciente) {
+      return res.status(404).json({ msj: "No existe paciente" });
+    }
+
+    if (paciente.veterinarioId != req.veterinario.id) {
+      return res
+        .status(404)
+        .json({ msj: "No puedes eliminar pacientes que no son tuyos" });
+    }
+
+    await paciente.destroy();
+
+    res.status(200).json({ msj: "Paciente eliminado correctamente" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export {
+  obtenerPacientes,
+  obtenerPaciente,
+  agregarPaciente,
+  actualizarPaciente,
+  eliminarPaciente,
+};
--- Back-End/Routers/pacientesRouter.js
+++ Back-End/Routers/pacientesRouter.js
@@ -1,18 +1,19 @@
 import express from "express"
 import auth from "../Middleware/authMiddlewere.js";
 import {
-    obtenerPacientes,
-    obtenerPaciente,
-    agregar
-} from "../Controllers/pacientes.js"
-
+  obtenerPacientes,
+  obtenerPaciente,
+  agregarPaciente,
+  actualizarPaciente,
+  eliminarPaciente,
+} from "../Controllers/pacientes.js";
 
 const router = express.Router();
 
-
-router.get("/", auth, obtenerPacientes)
-router.get("/:pacienteId", auth, obtenerPaciente)
-router.post("/agregar", auth, agregar)
-
+router.get("/", auth, obtenerPacientes);
+router.get("/:pacienteId", auth, obtenerPaciente);
+router.post("/agregar", auth, agregarPaciente);
+router.put("/actualizar/:id", auth, actualizarPaciente);
+router.delete("/eliminar/:id", auth, eliminarPaciente);
 
 export default router
